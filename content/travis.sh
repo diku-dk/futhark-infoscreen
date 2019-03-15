0dcc659335692ae3177dbb71c75df4bfc4a006f5
Niels G. W. Serup <ngws@metanohi.name>
2019-03-15 15:48:40 +0100

    Make the Travis slide work.

--- content/travis.sh
+++ content/travis.sh
@@ -1,4 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-surf -z 2.8 -r <(echo 'window.setTimeout(function() {window.scroll(0, 300);}, 1000);') \
-     https://travis-ci.org/diku-dk/futhark
+set -e
+
+scriptfile="$(mktemp)"
+trap 'rm "$scriptfile; exit"' INT QUIT TERM
+
+cat > "$scriptfile" <<EOF
+window.setTimeout(function() {
+  window.scroll(0, 300);
+}, 1000);
+EOF
+
+surf -p -b -F -z 2.8 -r $scriptfile https://travis-ci.org/diku-dk/futhark
