22d0fbb065cad962add7945d5c9ec3f07414897f
Niels G. W. Serup <ngws@metanohi.name>
2019-03-15 15:51:25 +0100

    This is not how you are supposed to program.

--- content/travis.sh
+++ content/travis.sh
@@ -8,7 +8,7 @@ trap 'rm "$scriptfile; exit"' INT QUIT TERM
 cat > "$scriptfile" <<EOF
 window.setTimeout(function() {
   window.scroll(0, 300);
-}, 1000);
+}, 5000);
 EOF
 
 surf -p -b -F -z 2.8 -r $scriptfile https://travis-ci.org/diku-dk/futhark
